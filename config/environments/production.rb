ab78fee5cae2b9c73197064c0e540d66f3802d8f
Caleb <calebcoates2468@att.net>
2016-09-12 14:42:38 -0700

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 
   # Required for Heroku 
   # Note to set this to your actual host
