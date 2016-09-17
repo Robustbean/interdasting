6e3dc1d10f86731cdb9fc3e615d56cb4da61bd9a
Caleb <calebcoates2468@att.net>
2016-09-16 17:55:11 -0700

    Cleaned up the pins scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
-  has_many :pins, dependent: :destroy
-
-  validates :name, presence: true
  end
 
